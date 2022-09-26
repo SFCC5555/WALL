f0ce69bd16bacbd8e1d7f8a89d52c948fb67d98b
sfcc5555 <sfcc.5555@gmail.com>
2022-09-26 18:36:32 -0500

    Ver 2.7, Calculos para Revisión del Deslizamiento

--- muros.js
+++ muros.js
@@ -1,3 +1,5 @@
+/*Parametros de Suelo 1 y 2*/
+
 let peuno=document.getElementById("pesoespecificouno");
 let pedos=document.getElementById("pesoespecificodos");
 
@@ -6,6 +8,8 @@ let afdos=document.getElementById("angulodefricciondos");
 
 let cuno=document.getElementById("cohesionuno");
 let cdos=document.getElementById("cohesiondos");
+
+/* Dimensiones propuestas del Muro */
 
 let h=document.getElementById("H");
 let htres=document.getElementById("Htres");
@@ -16,12 +20,17 @@ let btres=document.getElementById("Btres");
 let a=document.getElementById("a");
 let d=document.getElementById("D");
 
+/* Dimensiones calculadas del Muro */
+
 let hp=document.getElementById("Hp");
 let huno=document.getElementById("Huno");
 let hdos=document.getElementById("Hdos");
 let bcuatro=document.getElementById("Bcuatro");
 let bcinco=document.getElementById("Bcinco");
 let bseis=document.getElementById("Bseis");
+
+
+/* Parametros calculados  para revision del volcamiento*/
 
 let ka=document.getElementById("valorka");
 let pa=document.getElementById("valorpa");
@@ -58,9 +67,33 @@ let smo=document.getElementById("sumatoriamo");
 let fsv=document.getElementById("factorseguridadvolteo");
 let fsvok=document.getElementById("fsvok");
 
+/* Parametros calculados  para revision del deslizamiento*/
+
+let kp=document.getElementById("valorkp");
+let pp=document.getElementById("valorpp");
+let fsd=document.getElementById("factorseguridaddeslizamiento");
+let fsdok=document.getElementById("fsdok");
+
+/* Funcion para ambio de estilo de los resultados*/
+
 let z=document.querySelectorAll(".cambio");
 
+function cambio() {
+    for ( i=0; i<z.length; i++){
+        z[i].setAttribute("class","result");
+    }
+}
+
+/* Funcion para mostrar menu */
+
 let menu=document.querySelector("#menuContainer");
+
+function mostrarMenu() {
+    menu.classList.toggle("menu")
+}
+
+
+/* Funciones para la revision del Volcamiento */
 
 function calculo() {
 
@@ -82,11 +115,12 @@ function calculo() {
     xhp=parseFloat(h.value)+xhuno;
     hp.innerHTML=xhp.toFixed(2);
 
-    volteocoulomb()
-    cambio()
+    volteoCoulomb();
+    deslizamientoCoulomp();
+    cambio();
 }
 
-function volteocoulomb() {
+function volteoCoulomb() {
     alfa=parseFloat(a.value)*Math.PI/180;
     teta=15*Math.PI/180;
     fi=parseFloat(afuno.value)*Math.PI/180;
@@ -96,111 +130,134 @@ function volteocoulomb() {
     xka2=(Math.pow(Math.cos(teta),2))*(Math.cos(delta+teta));
     xka3=(Math.pow(1+Math.sqrt((Math.sin(delta+fi)*Math.sin(fi-alfa))/(Math.cos(delta+teta)*Math.cos(teta-alfa))),2));
     xkat=xka1/(xka2*xka3);
-    ka.innerHTML=xkat.toFixed(3);
+    ka.innerText=xkat.toFixed(3);
 
     xpa=0.5*(parseFloat(peuno.value))*(Math.pow(xhp,2))*(xkat);
-    pa.innerHTML=xpa.toFixed(2)+" KN/m";
+    pa.innerText=xpa.toFixed(2)+" KN/m";
 
     xpv=(xpa)*(Math.sin(teta+delta));
-    pv.innerHTML=xpv.toFixed(2)+" KN/m";
+    pv.innerText=xpv.toFixed(2)+" KN/m";
 
     xph=(xpa)*(Math.cos(teta+delta));
-    ph.innerHTML=xph.toFixed(2)+" KN/m";
+    ph.innerText=xph.toFixed(2)+" KN/m";
 
     momentos()
-    ok()
+    okVolcamiento()
 }
 
 function momentos() {
     
     xauno=(buno.value)*(xhdos);
-    auno.innerHTML=xauno.toFixed(2);
+    auno.innerText=xauno.toFixed(2);
 
     xados=(btres.value)*(xhdos)/2;
-    ados.innerHTML=xados.toFixed(2);
+    ados.innerText=xados.toFixed(2);
 
     xatres=(b.value)*(htres.value);
-    atres.innerHTML=xatres.toFixed(2);
+    atres.innerText=xatres.toFixed(2);
 
     xacuatro=(xbcuatro)*(xhdos)/2;
-    acuatro.innerHTML=xacuatro.toFixed(2);
+    acuatro.innerText=xacuatro.toFixed(2);
 
     xpuno=(xauno)*23.58;
-    puno.innerHTML=xpuno.toFixed(2);
+    puno.innerText=xpuno.toFixed(2);
 
     xpdos=(xados)*23.58;
-    pdos.innerHTML=xpdos.toFixed(2);
+    pdos.innerText=xpdos.toFixed(2);
 
     xptres=(xatres)*23.58;
-    ptres.innerHTML=xptres.toFixed(2);
+    ptres.innerText=xptres.toFixed(2);
 
     xpcuatro=(xacuatro)*23.58;
-    pcuatro.innerHTML=xpcuatro.toFixed(2);
+    pcuatro.innerText=xpcuatro.toFixed(2);
 
-    pver.innerHTML=xpv.toFixed(2);
+    pver.innerText=xpv.toFixed(2);
 
     xbxuno=parseFloat(bdos.value)+parseFloat(btres.value)+(parseFloat(buno.value)/2);
-    bxuno.innerHTML=xbxuno.toFixed(2)
+    bxuno.innerText=xbxuno.toFixed(2)
 
     xbxdos=parseFloat(bdos.value)+(parseFloat(btres.value)*2/3);
-    bxdos.innerHTML=xbxdos.toFixed(2);
+    bxdos.innerText=xbxdos.toFixed(2);
 
     xbxtres=parseFloat(b.value)/2
-    bxtres.innerHTML=xbxtres.toFixed(2);
+    bxtres.innerText=xbxtres.toFixed(2);
 
     xbxcuatro=parseFloat(bdos.value)+parseFloat(btres.value)+parseFloat(buno.value)+(parseFloat(xbcuatro)/3);
-    bxcuatro.innerHTML=xbxcuatro.toFixed(2);
+    bxcuatro.innerText=xbxcuatro.toFixed(2);
 
     xbxv=parseFloat(bdos.value)+parseFloat(btres.value)+parseFloat(buno.value)+((xhp)*(2/3)*(Math.tan(teta)));
-    bxv.innerHTML=xbxv.toFixed(2);
+    bxv.innerText=xbxv.toFixed(2);
 
     xmuno=(xbxuno)*(xpuno);
-    muno.innerHTML=xmuno.toFixed(2);
+    muno.innerText=xmuno.toFixed(2);
 
     xmdos=(xbxdos)*(xpdos);
-    mdos.innerHTML=xmdos.toFixed(2);
+    mdos.innerText=xmdos.toFixed(2);
 
     xmtres=(xbxtres)*(xptres);
-    mtres.innerHTML=xmtres.toFixed(2);
+    mtres.innerText=xmtres.toFixed(2);
 
     xmcuatro=(xbxcuatro)*(xpcuatro);
-    mcuatro.innerHTML=xmcuatro.toFixed(2);
+    mcuatro.innerText=xmcuatro.toFixed(2);
 
     xmv=(xbxv)*(xpv);
-    mv.innerHTML=xmv.toFixed(2);
+    mv.innerText=xmv.toFixed(2);
 
 
     xsv=(xpuno)+(xpdos)+(xptres)+(xpcuatro)+(xpv);
-    sv.innerHTML=xsv.toFixed(2);
+    sv.innerText=xsv.toFixed(2);
 
     xsm=(xmuno)+(xmdos)+(xmtres)+(xmcuatro)+(xmv);
-    sm.innerHTML=xsm.toFixed(2);
+    sm.innerText=xsm.toFixed(2);
 
     xsmo=(xph)*((xhp)/3);
-    smo.innerHTML=xsm.toFixed(2);
+    smo.innerText=xsm.toFixed(2);
 
     xfsv=(xsm)/(xsmo);
-    fsv.innerHTML=xfsv.toFixed(2)
+    fsv.innerText=xfsv.toFixed(2);
 
 }
 
-function cambio() {
-    for ( i=0; i<z.length; i++){
-        z[i].setAttribute("class","result");
-    }
-}
 
-function ok() {
+
+function okVolcamiento() {
     if (xfsv>=3) {
-        fsvok.innerHTML="OK"
-        fsvok.setAttribute("class","result");
+        fsvok.innerText="OK";
+        fsvok.setAttribute("class","contenedorResultado");
     }
     else {
-        fsvok.innerHTML="Cambiar dimenciones"
+        fsvok.innerText="Cambiar dimenciones";
         fsvok.setAttribute("class","error");
     }
 }
 
-function mostrarMenu() {
-    menu.classList.toggle("menu")
+
+/* Funciones para la revision del Deslizamiento */
+
+function deslizamientoCoulomp() {
+
+    fidos=parseFloat(afdos.value)*Math.PI/180;
+    xkp=Math.pow(Math.tan((45*Math.PI/180) + (fidos/2)),2);
+    kp.innerText=xkp.toFixed(3)
+
+    xpp=((1/2)*(parseFloat(pedos.value))*(Math.pow(parseFloat(d.value),2))*(xkp))+(2*(parseFloat(cdos.value))*(parseFloat(d.value))*(Math.sqrt(xkp)));
+    pp.innerText=xpp.toFixed(2)+" KN/m";
+
+    k1=(2/3);
+    k2=(2/3);
+    k1rad=k1*Math.PI/180;
+
+    xfsd=((xsv)*(Math.tan((k1)*(fidos)))+((parseFloat(b.value))*(k2)*(parseFloat(cdos.value)))+(xpp))/(xph);
+    fsd.innerText=xfsd.toFixed(2);
+
+    if (xfsd>=1.6) {
+        fsdok.innerText="OK";
+        fsdok.setAttribute("class","contenedorResultado");
+    }
+    else {
+        fsdok.innerText="Cambiar dimenciones";
+        fsdok.setAttribute("class","error");
+    }
+
 }
+
